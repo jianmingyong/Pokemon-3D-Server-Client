c225d02ee1b0c78eac0d43d09d38cbc3cbd91d4c
Yong Jian Ming <jianming1993@gmail.com>
2025-12-06 01:23:14 +0800

    Update

--- Pokemon.3D.Server.Client.Updater/Program.cs
+++ Pokemon.3D.Server.Client.Updater/Program.cs
@@ -3,40 +3,51 @@ using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using SharpCompress.Common;
-using SharpCompress.Reader;
+using SharpCompress.Readers;
 
 namespace Pokemon_3D_Server_Client_Updater
 {
-    /// <summary>
-    /// Class containing the Main Access point.
-    /// </summary>
     public class Program
     {
-        /// <summary>
-        /// Main Access Point.
-        /// </summary>
-        /// <param name="args">Program Start Argument.</param>
         public static void Main(string[] args)
         {
-            Thread.Sleep(5000);
-
-            if (args.GetLength(0) > 0 && Directory.Exists(args[0].Replace("%20", " ")))
+            if (args.Length > 0 && Directory.Exists(args[0].Replace("%20", " ")))
             {
+                Console.WriteLine("Updating Pokemon 3D Server...");
+                
+                if (args.Length > 1)
+                {
+                    try
+                    {
+                        Process.GetProcessById(int.Parse(args[1])).WaitForExit();
+                    }
+                    catch (ArgumentException e)
+                    {
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(5000);
+                }
+                
                 try
                 {
                     using (Stream stream = File.OpenRead(args[0].Replace("%20", " ") + "\\Pokemon.3D.Server.Client.GUI.zip"))
                     {
                         var reader = ReaderFactory.Open(stream);
+
                         while (reader.MoveToNextEntry())
                         {
                             try
                             {
                                 if (!reader.Entry.IsDirectory)
                                 {
-                                    reader.WriteEntryToDirectory(args[0].Replace("%20", " "), ExtractOptions.ExtractFullPath | ExtractOptions.Overwrite);
+                                    reader.WriteEntryToDirectory(args[0].Replace("%20", " "), new ExtractionOptions {ExtractFullPath = true, Overwrite = true});
                                 }
                             }
-                            catch (Exception) { }
+                            catch (Exception)
+                            {
+                            }
                         }
                     }
 
@@ -45,7 +56,9 @@ namespace Pokemon_3D_Server_Client_Updater
                         File.Delete(args[0].Replace("%20", " ") + "\\Pokemon.3D.Server.Client.GUI.zip");
                     }
                 }
-                catch (Exception) { }
+                catch (Exception)
+                {
+                }
 
                 Process.Start(args[0].Replace("%20", " ") + "\\Pokemon.3D.Server.Client.GUI.exe");
             }
